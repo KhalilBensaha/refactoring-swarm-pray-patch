8318ca2f22c96664062d289eceb7add83fffa2dc
KhalilBensaha <nk_bensaha@esi.dz>
2026-01-28 00:11:30 +0100

    adding fixer agent skeleton

--- main.py
+++ main.py
@@ -2,30 +2,21 @@ import argparse
 import sys
 import os
 from dotenv import load_dotenv
-##from src.utils.logger import log_experiment
+
 from src.agents.auditor_agent import run_auditor
+from src.agents.fixer_agent import fixer_agent
 
 load_dotenv()
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description="Refactoring Swarm")
-    parser.add_argument(
-        "--target_dir",
-        type=str,
-        required=True,
-        help="Directory containing Python files"
-    )
-    return parser.parse_args()
-
 def list_python_files(target_dir):
-    python_files = []
-    for filename in os.listdir(target_dir):
-        if filename.endswith(".py"):
-            python_files.append(os.path.join(target_dir, filename))
-    return python_files
+    return [
+        os.path.join(target_dir, f)
+        for f in os.listdir(target_dir)
+        if f.endswith(".py")
+    ]
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Refactoring Swarm")
     parser.add_argument("--target_dir", type=str, required=True)
     args = parser.parse_args()
 
@@ -33,17 +24,11 @@ def main():
         print(f"❌ Dossier {args.target_dir} introuvable.")
         sys.exit(1)
 
-    print(f"🚀 DEMARRAGE SUR : {args.target_dir}")
-    ##log_experiment("System", "STARTUP", f"Target: {args.target_dir}", "INFO")
-    print("✅ MISSION_COMPLETE")
-
     python_files = list_python_files(args.target_dir)
 
     if not python_files:
         print("⚠️ No Python files found.")
         sys.exit(0)
-
-    
 
     print("🚀 Starting Refactoring Swarm")
     print("Python files detected:")
@@ -51,9 +36,13 @@ def main():
         print("-", file)
 
     print("\n🔍 Running Auditor on first file...\n")
-    result = run_auditor(python_files[0])
-    print(result)
+    analysis_result = run_auditor(python_files[0])
+    print(analysis_result)
 
+    print("\n🛠 Running Fixer...\n")
+    fixer_agent(analysis_result, args.target_dir)
+
+    print("\n✅ Refactoring Swarm completed")
 
 if __name__ == "__main__":
     main()
